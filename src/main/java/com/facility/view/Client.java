e0344f8f21f533c68ab04ac3d3cf28c94e1b5fb2
Chrissy Soulakian <csoulakian@luc.edu>
2016-04-02 01:01:30 -0500

    fixed javadocs

--- src/main/java/com/facility/view/Client.java
+++ src/main/java/com/facility/view/Client.java
@@ -2,18 +2,18 @@ package com.facility.view;
 
 public class Client {
 
-	/**
+	/***
 	 * Important! Can only run one client at a time due to connection limitations on Heroku database.
 	 * Must comment out the other two unused clients in this main method in Client class.
-	 * @param args
-	 * @throws Exception
+	 * @param args args
+	 * @throws Exception if there is an error creating the particular client
 	 */
 	@SuppressWarnings("unused")
 	public static void main(String[] args) throws Exception {
 		
-		//FacilityClient facilityClient = new FacilityClient();
+		FacilityClient facilityClient = new FacilityClient();
 		//UseClient useClient = new UseClient();
-		MaintenanceClient maintenanceClient = new MaintenanceClient();
+		//MaintenanceClient maintenanceClient = new MaintenanceClient();
 
 	}
 
